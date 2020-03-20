055b1e74ba6e28f0d3204e63a292ae6418475a83
Liam <CrawfordAndWood@mail.com>
2020-03-20 16:23:49 +0000

    route setup

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-//@route    GET api/users
+//@route    GET api/dashboard
 //@desc     Test route
 //@access   Public
-router.get("/", (req, res) => res.send("User route"));
+router.get("/", (req, res) => res.send("dashboard route"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,13 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/streets", require("./routes/api/streets"));
+app.use("/api/dashboard", require("./routes/api/dashboard"));
+app.use("/api/inventory", require("./routes/api/inventory"));
+app.use("/api/households", require("./routes/api/households"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
