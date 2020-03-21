a306c85b26ae75eb2b8f483e7ad0a480605cb57e
Liam <CrawfordAndWood@mail.com>
2020-03-21 22:39:28 +0000

    user profile crud

---
+++ routes/api/profile.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+//@route    GET api/profile/me
+//@desc     Get current user's profile
+//@access   Private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "No User Profile exists" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.messge);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route    POST api/profile
+//@desc     Create or update user profile
+//@access   Private
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("role", "Role is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { role } = req.body;
+
+    //Build profile
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (role) profileFields.role = role;
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          {
+            new: true
+          }
+        );
+
+        return res.json(profile);
+      }
+
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.messge);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 app.use("/api/users", require("./routes/api/users"));
@@ -13,6 +16,7 @@ app.use("/api/streets", require("./routes/api/streets"));
 app.use("/api/dashboard", require("./routes/api/dashboard"));
 app.use("/api/inventory", require("./routes/api/inventory"));
 app.use("/api/households", require("./routes/api/households"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 const PORT = process.env.PORT || 5000;
 
