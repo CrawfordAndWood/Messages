cc4549bf64c8cb1121c1b665806fffbd67f351fd
Liam <CrawfordAndWood@mail.com>
2020-03-29 22:32:09 +0100

    alert buttons

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckCircle,
+  faTimesCircle
+} from "@fortawesome/free-solid-svg-icons";
 
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map(alert => (
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-      {alert.msg}
-    </div>
+    <Fragment>
+      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+        <FontAwesomeIcon
+          icon={alert.alertType === "success" ? faCheckCircle : faTimesCircle}
+        />
+        {alert.msg}
+      </div>
+    </Fragment>
   ));
 
 Alert.propTypes = {
