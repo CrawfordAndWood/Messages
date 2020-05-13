108e1fb13ec830a2aa6d744959ce7d5be3141743
Liam <CrawfordAndWood@mail.com>
2020-05-13 10:53:40 +0100

    dashboard update

--- client/src/components/dashboard/GlobalAdminDashboard.js
+++ client/src/components/dashboard/GlobalAdminDashboard.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import DashboardActions from "./DashboardActions";
 import {
   faUser,
   faUserFriends,
